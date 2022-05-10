38393d067a92bb67afe1345382dc3208165e4f33
europ352109 <europa352109@gmail.com>
2022-05-10 10:39:25 +0900

    re

---
+++ data structure/exercise_5-4.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int element;
+
+typedef struct stackNode {
+	element data;
+	struct stackNode* link;
+}stackNode;
+
+stackNode* top;
+
+int Empty() {
+	if (top == NULL) return 1;
+	else return 0;
+}
+
+void push(element item) {
+	stackNode* temp = (stackNode*)malloc(sizeof(stackNode));
+	temp->data = item;
+	temp->link = top;
+	top = temp;
+}
+
+element pop() {
+	element item;
+	stackNode* temp = top;
+
+	if (top == NULL) {
+		printf("\n\n Stack is empty !\n");
+		return 0;
+	}
+	else {
+		item = temp->data;
+		top = temp->link;
+		free(temp);
+		return item;
+	}
+}
+
+element evalPostfix(const char* exp) {
+	int opr1, opr2, value, i = 0;
+	int length = strlen(exp);
+	char symbol;
+	top = NULL;
+
+	for (i = 0; i < length; i++) {
+		symbol = exp[i];
+		if (symbol != '+' && symbol != '-' && symbol != '*' && symbol != '/') {
+			value = symbol - '0';
+			push(value);
+		}
+		else {
+			opr2 = pop();
+			opr1 = pop();
+			switch (symbol) {
+			case '+': push(opr1 + opr2); break;
+			case '-': push(opr1 - opr2); break;
+			case '*': push(opr1 * opr2); break;
+			case '/': push(opr1 / opr2); break;
+			}
+		}
+	}
+	return pop();
+}
+
+void main() {
+	int result;
+	const char* express = "35*62/-";
+	printf("후위 표기식 : %s", express);
+
+	result = evalPostfix(express);
+	printf("\n\n연산 결과 => %d", result);
+
+	getchar();
+}
